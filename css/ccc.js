3ea4ba0ca383bf6d7044c16e73ef3cc78ec5a6eb
Chrissy Wainwright <chrissy@sixfeetup.com>
2015-08-07 21:49:08 -0400

    initial commit

---
+++ css/ccc.js
@@ -0,0 +1,6 @@
+(function($) { $(function() {
+    // mobile - display navigation/search when toggle button is clicked
+    $("#nav-toggle").click(function(){
+        $("#nav").toggleClass("displayNav");
+    });
+}); })(jQuery);
