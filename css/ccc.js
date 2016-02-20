59db5136548565f02bddaa8301a0e6e6b925d13a
Chrissy Wainwright <chrissy@sixfeetup.com>
2016-02-19 21:00:21 -0600

    fix audio file loading, make sure plain sermon page keeps old styles

--- css/ccc.js
+++ css/ccc.js
@@ -18,13 +18,8 @@
     $("#sermons li span").hover(function() {
         $("#sermons li span").removeClass("selected");
         $(this).addClass("selected");
-        $("#audio h3").text($(this).attr("data-title"));
-        $("#audio .date").text($(this).attr("data-datefull"));
-        $("#audio .series").text("Series: " + $(this).attr("data-series"));
-        
         datenum = $(this).attr("data-datenum");
-        $("#audio source").attr("src", "sermons/" + datenum + ".m4a");
-        $("#audio param[name='flashvars']").attr("value", "file=sermons/" + datenum + ".m4a");
-        $("#audio .downloadlink").attr("href", "sermons/" + datenum + ".m4a");
+        $("#audio .selected").removeClass("selected");
+        $("#audio [data-date='" + datenum + "']").addClass("selected");
     });
 }); })(jQuery);
