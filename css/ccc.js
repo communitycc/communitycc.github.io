be14b1c0834064eafb9e3b3b24af1e95ab812fc7
Chrissy Wainwright <chrissy@sixfeetup.com>
2016-04-09 20:48:55 -0500

    use forms for requesting info or sending prayer requests

--- css/ccc.js
+++ css/ccc.js
@@ -27,4 +27,45 @@
         $("#audio .selected").removeClass("selected");
         $("#audio [data-date='" + datenum + "']").addClass("selected");
     });
+
+    // display forms
+    function displayForm(formID) {
+        $("body").addClass("formOpen");
+        $(formID).show();
+    }
+    $(".infoLink").click(function(){
+        displayForm("#info-form");
+        return false;
+    })
+    $(".prayerLink").click(function(){
+        displayForm("#prayer-form");
+        return false;
+    })
+
+    // close when clicking outside form or hitting escape
+    $(document).on('keyup',function(evt) {
+        if (evt.keyCode == 27) {
+            $(".overlayForm").hide();
+            $("body").removeClass("formOpen");
+        }
+    });
+    $(".closeOverlay").click(function(){
+        $(".overlayForm").hide();
+        $("body").removeClass("formOpen");
+    });
+
+    // form submission validation
+    $(".overlayForm input[type='submit']").click(function(){
+        if ($(this).siblings("input[name='name']").val() == "") {
+            window.alert("Please enter your name");
+            return false;
+        } else if ($(this).siblings("textarea[name='prayer_request']").val() == "") {
+            window.alert("Please enter your prayer request");
+            return false;
+        } else if ($(this).siblings("textarea[name='comments']").val() == "") {
+            window.alert("Please enter your questions or comments");
+            return false;
+        }
+
+    });
 }); })(jQuery);
